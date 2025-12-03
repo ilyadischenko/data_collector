d9b808c5535bd74b8cfcd32255b7f1d007f4b6ab
IlyaDischenko <ilyadischenko@gmail.com>
2025-12-03 13:08:45 +0300

    3

---
+++ cloud.py
@@ -0,0 +1,19 @@
+
+
+
+key = "YCAJEVJeIO1bNwwm7wm9o9by1"
+secret_key = "YCNJ_6Frr8TWLFZxASFW47ZeYGFTtEawQaE0gwXa"
+
+
+
+
+class DataCloud:
+    def __init__(self):
+        self.url = "https://storage.yandexcloud.net/data-collector-hft"
+        self.key = key
+        self.secret_key = secret_key
+
+    
+
+    def get_all_objects(self):
+        pass
